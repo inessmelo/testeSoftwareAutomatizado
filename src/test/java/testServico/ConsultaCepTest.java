2d1f782d828808750efa40e30588b08535313f3e
Ines Melo <prof.inesmelo@gmail.com>
2020-10-27 20:57:12 -0300

    Criando métodos na classe

--- src/test/java/testServico/ConsultaCepTest.java
+++ src/test/java/testServico/ConsultaCepTest.java
@@ -10,6 +10,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class ConsultaCepTest {
 
@@ -22,13 +23,11 @@ public class ConsultaCepTest {
 		String cep = "26260230";
 		String endpoint = cep.concat("/json");
 
-		//setando a url
-		RestAssured.baseURI = url;
+		RestAssured.baseURI = url;  //setando a URL
 
-		RestAssured.given().relaxedHTTPSValidation()
-		.contentType(ContentType.JSON)
-		.when().get(endpoint)
-		.then().statusCode(200);
+		Response response = get(endpoint);   //chamando o método criado
+		
+		assertEquals(200, response.statusCode()); //validando o status do response
 	}
 	
 	@Test
@@ -36,21 +35,19 @@ public class ConsultaCepTest {
 		
 		String cep = "26260230";
 		String endpoint = cep.concat("/json");
-		LinkedHashMap<String, String> cabecalho = new LinkedHashMap<String, String>();
-		cabecalho.put("cliente_id", "cep");
-		cabecalho.put("Authorization", "Basic aW5lc21lbG86MTIzNDU=");
+		LinkedHashMap<String, String> header = new LinkedHashMap<String, String>();
+		header.put("cliente_id", "cep");
+		header.put("Authorization", "Basic aW5lc21lbG86MTIzNDU=");
 		
-		//setando a url
-		RestAssured.baseURI = url;
+		RestAssured.baseURI = url;  //setando a URL
+		//ResponseSpecification request = initRequest(ContentType.JSON, header); //chamando o método e passando o tipo do contentType
 		
 		//pegando tudo que está no response
-		Response response = RestAssured.given()
-				.relaxedHTTPSValidation()  //evita o SSL
-				.contentType(ContentType.JSON) 
-				.headers(cabecalho)     //o cabeçalho identificado acima
-				.when().get(endpoint)   //fazendo um GET do cep
-				.then().statusCode(200) //status igual a 200
-				.extract().response();  //extraindo todo o response
+		Response response = initRequest(ContentType.JSON)
+				.headers(header)
+				.when().get(endpoint)     //fazendo um GET do cep
+				.then().statusCode(200)   //validando o status 
+				.extract().response();    //extraindo todo o response
 		
 		//seleciona o body do response
 		JsonPath json = response.getBody().jsonPath();
@@ -65,21 +62,18 @@ public class ConsultaCepTest {
 		
 		String cep = "26260230";
 		String endpoint = cep.concat("/json");
-		LinkedHashMap<String, String> parametro = new LinkedHashMap<String, String>();
-		parametro.put("cliente_id", "cep");
-		parametro.put("Authorization", "Basic aW5lc21lbG86MTIzNDU=");
+		LinkedHashMap<String, String> param = new LinkedHashMap<String, String>();
+		param.put("cliente_id", "cep");
+		param.put("Authorization", "Basic aW5lc21lbG86MTIzNDU=");
 		
-		//setando a url
-		RestAssured.baseURI = url;
+		RestAssured.baseURI = url;  //setando a URL
 		
 		//pegando tudo que está no response
-		Response response = RestAssured.given()
-				.relaxedHTTPSValidation()  //desconsidera se tiver HTTPS
-				.contentType(ContentType.JSON) //tipo de envio (JSON)
-				.params(parametro)      //add os paramentros identificado acima
-				.when().get(endpoint)   //fazendo um GET do cep e clicar no SEND
-				.then().statusCode(200) //validando o status 
-				.extract().response();  //extraindo todo o response
+		Response response = initRequest(ContentType.JSON) //chamando o método e passando o tipo do contentType
+				.params(param)        //add os paramentros identificado acima
+				.when().get(endpoint)     //fazendo um GET do cep e clicar no SEND
+				.then().statusCode(200)   //validando o status 
+				.extract().response();    //extraindo todo o response
 		
 		//seleciona o body do response e pega o JSON
 		JsonPath json = response.getBody().jsonPath();
@@ -87,6 +81,30 @@ public class ConsultaCepTest {
 		//o que se espera na consulta do response
 		assertEquals("Rua Dom Manuel", json.get("logradouro"));
 		assertEquals("Nova Iguaçu", json.get("localidade"));
+	}
+	
+	//criando método para contentType 
+	public RequestSpecification initRequest(ContentType contentType) {
+		return RestAssured.given()
+		.relaxedHTTPSValidation()      //desconsidera se tiver HTTPS
+		.contentType(contentType);     //tipo de envio (JSON)
+	}
+	
+	//criando método para o HEADER
+	public RequestSpecification initRequest(ContentType contentType, LinkedHashMap<String, String> header) {
+		return RestAssured.given()
+		.relaxedHTTPSValidation()      //desconsidera se tiver HTTPS
+		.contentType(contentType)      //tipo de envio (JSON)
+		.headers(header);           //validando o header
+	}
+		
+	//criando método para p RESPONSE
+	public Response get(String endpoint) {
+		return initRequest(ContentType.JSON) //chamando o método e passando o tipo do contentType		
+		.when().get(endpoint)     //fazendo um GET do cep e clicar no SEND
+		.then()
+		.extract().response();   //extraindo todo o response 
+
 	}
 
 }
